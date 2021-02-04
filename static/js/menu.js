12cc1f3eef61ce514e40fda3de9d2b19bef990d9
FranciscoRey <franciscorey98@gmail.com>
2021-02-04 15:32:34 -0300

    navbar servicios particulares

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,23 +1,50 @@
-/*===== MENU SHOW Y HIDDEN =====*/ 
-const navMenu = document.getElementById('nav-menu'),
-      toggleMenu = document.getElementById('nav-toggle'),
-      closeMenu = document.getElementById('nav-close')
+/*===== EXPANDER MENU  =====*/ 
+const showMenu = (toggleId, navId)=>{
+  const toggle = document.getElementById(toggleId),
+  nav = document.getElementById(navId)
 
-/*SHOW*/ 
-toggleMenu.addEventListener('click', ()=>{
-    navMenu.classList.toggle('show')
-})
-
-/*HIDDEN*/
-closeMenu.addEventListener('click', ()=>{
-    navMenu.classList.remove('show')
-})
+  if(toggle && nav){
+    toggle.addEventListener('click', ()=>{
+      nav.classList.toggle('show')
+      toggle.classList.toggle('bx-x')
+    })
+  }
+}
+showMenu('header-toggle','nav-menu')
 
 /*===== ACTIVE AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');   
 
 function linkAction(){
-  /*Remove menu mobile*/
-  navMenu.classList.remove('show')
+/*Active link*/
+navLink.forEach(n => n.classList.remove('active'));
+this.classList.add('active');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+/*===== MENU SHOW Y HIDDEN =====*/ 
+/*const navMenu = document.getElementById('nav-menu'),
+      toggleMenu = document.getElementById('nav-toggle'),
+      closeMenu = document.getElementById('nav-close')
+*/
+/*SHOW*/ 
+/*toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+*/
+/*HIDDEN*/
+/*
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+*/
+/*===== ACTIVE AND REMOVE MENU =====*/
+
+/*const navLink = document.querySelectorAll('.nav__link');   
+*/
+/*function linkAction(){*/
+  /*Remove menu mobile*/
+  /*navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));*/
